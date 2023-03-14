87bb1b5439b27c913408348fc57e2716e4ab9556
Dudu da Rocha <eduardoldarocha@gmail.com>
2023-03-13 21:20:37 -0300

    feat: adicionado tema claro

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
+import { ThemeProvider } from "@mui/material";
 import { BrowserRouter } from "react-router-dom";
 import { AppRoutes } from "./routes";
+import { LigthTheme } from "./shared/themes";
 
 export const App = () => {
   return (
     <BrowserRouter>
-      <AppRoutes/>
+    <ThemeProvider theme={LigthTheme}>
+      <AppRoutes />
+    </ThemeProvider>
     </BrowserRouter>
   );
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,10 @@
+import { Button } from "@mui/material";
 import { Navigate, Route, Routes } from "react-router-dom";
 
 export const AppRoutes = () => {
     return (
 <Routes>
-    <Route path="/pagina-inicial" element={<p>Página inicial</p>}></Route>
+    <Route path="/pagina-inicial" element={<Button variant='contained' color='primary'>Página inicial</Button>}></Route>
 
     <Route path="*" element={<Navigate to="/pagina-inicial"/>}></Route>
 </Routes>
